0b3acc1a3be2e1a44d233b6050fba5a9cdbfed45
imdc <garo567@hotmail.com>
2019-06-11 16:42:30 -0400

    I am not worthy of your presence

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,12 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-  ]
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
